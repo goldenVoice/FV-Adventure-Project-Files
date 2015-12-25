b0e98b7c29edf5ecab3923716e2aa4bbbb94b79b
goldenVoice <goldenvoice8@gmail.com>
2015-12-25 12:24:41 +0800

    new script for tutorial cause i started from scratch again

--- Assets/Scripts/TutorialManager1.cs
+++ Assets/Scripts/TutorialManager1.cs
@@ -8,7 +8,9 @@ using UnityEngine.EventSystems;
 public class TutorialManager1 : MonoBehaviour {
 	[HideInInspector]
 	public bool tutorial;
-	
+
+	bool kingGuavaEntered;
+
 	public Animator anim_kingGuava;
 	private int counter;
 	public GameObject[] buttons;	// sa start ng tutorial dapat naka disable/ not interactable to. para di makapag laro ang user, at makinig sa tutorial :P
@@ -21,6 +23,9 @@ public class TutorialManager1 : MonoBehaviour {
 	private Potholes_list list_potholes;
 	
 	private PotholeManager potholeManager;
+
+	public GameObject[]	potholes_array;
+	private BoxCollider2D[] colliders_pothole1;
 
 	void Awake(){
 		gameManager = (GameManagerBehavior) GameObject.FindObjectOfType(typeof(GameManagerBehavior));
@@ -41,20 +46,35 @@ public class TutorialManager1 : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		tutorial = true;
+		kingGuavaEntered = false;
 		counter = 0;
 		tutorialText.text = tutorial_dialog[counter];	// display the first tutorial text from the array tutorial_dialog
 		foreach(GameObject button in buttons){
 			button.SetActive(false);	// hide first the buttons
 		}
+		buttons[11].gameObject.SetActive(true);	// show first pothole
 		counter++;
 	}
 
 	void Update(){
 		if(tutorial){
-			if(anim_kingGuava.GetCurrentAnimatorStateInfo(0).IsName("king guava idle")
-			   && tutorialText.text != "Click on the carrot then click any pothole."){
+			if(anim_kingGuava.GetCurrentAnimatorStateInfo(0).IsName("king guava idle") && !kingGuavaEntered){
 				tutorialText.transform.parent.gameObject.SetActive(true);	// show the tutorial text;
 				buttons[10].SetActive(true);
+				kingGuavaEntered = true;	// set to true para di na daanan tong condition na to
+			}
+		//		tutorial_dialog.Add();	
+			else if(tutorialText.text ==  "I will give you my Veggie hero, the Carrot."){
+				colliders_pothole1 = buttons[11].GetComponents<BoxCollider2D>();
+				colliders_pothole1[0].enabled = false;		// disable both para di muna makapag tanim yung user :D
+				colliders_pothole1[1].enabled = false;		// 2 kase yung collider kaya nilagay ko sa array
+			}
+			else if(tutorialText.text == "Click on the carrot then click any pothole."){
+				buttons[10].SetActive(false);										// hide NextButton
+				if(gameManager.currentSelectedHero != null){							// if may hero dito (hindi null) means na click ng user, yung carrot hero
+					hideTutorialStuffs();
+					potholes_array[0].transform.GetChild(1).gameObject.SetActive(true);	// show pothole highlight
+				}
 			}
 
 		}
@@ -63,50 +83,40 @@ public class TutorialManager1 : MonoBehaviour {
 	public void tutorialNext(){
 		// if the tutorial is still on going
 		if(tutorial){
-			if(tutorialText.text == "The insects have arrived! Defend our place!" && counter == 1
-			   && (tutorialText.transform.parent.gameObject.activeSelf == true)){	// this check lets the user click anywhere ng hindi nasisira yung pattern ng tutorial. kase pag wala to, mag po proceed ang counter++, masisira yung tutorial, di pa tapos si king guava pumasok. pwede na mag plant
-				//buttons[0].SetActive(true);
-				tutorialText.text = tutorial_dialog[counter];
-				//show the carrot
-				buttons[0].SetActive(true);
-				buttons[0].transform.GetChild(1).gameObject.SetActive(false);
-				
-				// SHOW THE WATER BAR
-				buttons[5].SetActive(true);
-				
-				GameObject waterText = GameObject.Find("WaterBarText");
-				foreach(GameObject potholes in list_potholes.potholesList){					
-					potholes.GetComponent<PotholeManager>().waterText = waterText;
-				}
-				
-				counter++;
+			if(tutorialText.text ==  "The insects have arrived! Defend our place!"){
+				nextMessage();
+				buttons[0].SetActive(true);										// show the carrot
+				buttons[0].transform.GetChild(1).gameObject.SetActive(false);	// hide the highlight
 			}
-			// dialog: "I will give you my warrior, the carrot" 1
-			else if(tutorialText.text == "I will give you my Veggie hero, the Carrot." /* && counter == 1*/){
-				
-				tutorialText.text = tutorial_dialog[counter];
-				counter++;
+			else if(tutorialText.text ==  "I will give you my Veggie hero, the Carrot."){
+				nextMessage();
 			}
-			else if(tutorialText.text == "As shown below the Carrot's image, this hero costs 80 water."/* || counter == 2*/){
-				tutorialText.text = tutorial_dialog[counter];
-				buttons[0].transform.GetChild(1).gameObject.SetActive(true);
-				counter++;				
+			else if(tutorialText.text ==  "As shown below the Carrot's image, this hero costs 80 water."){
+				nextMessage();
+				showTutorialStuffs();
 			}
-			else if(tutorialText.text == "Click on the carrot then click any pothole." && counter == 3){
-				buttons[0].transform.GetChild(1).gameObject.SetActive(true);
-			}
-			// if the user didnt click on the pothole, he will see the instruction again
-			else if(tutorialText.text == "Click on the carrot then click any pothole." 
-			        && gameManager.currentSelectedHero == null && counter == 5){		
-				// disable the highlights of potholes
-				foreach(GameObject potholes in list_potholes.potholesList){
-					potholes.transform.GetChild(1).gameObject.SetActive(false);	// this is the highlight gameObject, referenced then setactive (true)
-				}
-				buttons[0].transform.GetChild(1).gameObject.SetActive(true);   // show the highlight of the carrot
-				anim_kingGuava.gameObject.GetComponent<Image>().enabled = true;					    // show king guava 
-				tutorialText.transform.parent.gameObject.SetActive(true);	    // show the tutorial text
-			}
+
 		}
 	}
-
+	
+	void hideTutorialStuffs(){
+		buttons[0].transform.GetChild(1).gameObject.SetActive(false);     // disable the highlight of the carrot
+		anim_kingGuava.gameObject.GetComponent<Image>().enabled = false;  // hide king guava 
+		tutorialText.transform.parent.gameObject.SetActive(false);	      // hide the tutorial text
+		buttons[10].SetActive(true);
+		
+	}
+		
+	void showTutorialStuffs(){
+		anim_kingGuava.gameObject.GetComponent<Image>().enabled = true;	 	 // show king guava 
+		buttons[0].transform.GetChild(1).gameObject.SetActive(true);     	 // show the highlight of the carrot
+		tutorialText.transform.parent.gameObject.SetActive(true);	    	 // show the tutorial text
+		buttons[10].SetActive(true);
+	}
+	
+	void nextMessage(){
+		Debug.Log ("dialog[" + counter + "]: " + tutorial_dialog[counter]);
+		tutorialText.text = tutorial_dialog[counter];
+		counter++;
+	}
 }

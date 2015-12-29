30a5f92d2439a92778831bb438d540687f99b4c9
goldenVoice <goldenVoice8@gmail.com>
2015-12-29 16:08:29 +0800

    Map: scrollable

--- Assets/Async Loading/LoadingScreen1.cs
+++ Assets/Async Loading/LoadingScreen1.cs
@@ -19,7 +19,7 @@ public class LoadingScreen1 : MonoBehaviour {
 	
 	private float progress = 0;
 	
-	AsyncOperation asyncop;
+	public AsyncOperation asyncop;
 	
 	// Use this for initialization
 	void Start () {
@@ -37,14 +37,16 @@ public class LoadingScreen1 : MonoBehaviour {
 
 		// start with the size of the progress bar as 0
 		progressBar.transform.localScale = new Vector3 (0f, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+		Debug.Log(isLoading);
+		Debug.Log(asyncop);
 		
 		asyncop.allowSceneActivation = false;		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(isLoading);
 		if(isLoading){		// if nag lo load na. 
+			Debug.Log ("LOAAAAAAAAAADING");
 			newProgress = asyncop.progress * origScale;		
 			textObj.text = "LOADING: " + ((int) (asyncop.progress * 100)) + "%";		// show current percentage, kaya cinast ko sa int para whole num lang
 			progressBar.transform.localScale = new Vector3 (newProgress, progressBar.transform.localScale.y, progressBar.transform.localScale.z);

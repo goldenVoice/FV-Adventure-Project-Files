1b7af35868f12755914275173ee9d2cb23195b22
goldenVoice <goldenVoice8@gmail.com>
2016-01-02 12:32:42 +0800

    inventory on stage area

---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour {
+
+	bool inventoryEnabled;
+	public Toggle inventoryToggle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void showInventory(){
+		if(inventoryToggle.GetComponent<Toggle>().isOn){	// naka on ang toggle
+			inventoryToggle.transform.GetChild(0).GetComponent<Image>().enabled = false;			
+			inventoryToggle.transform.GetChild(0).GetChild(0).GetComponent<Image>().enabled = true;
+			gameObject.GetComponent<Canvas>().enabled = true;			// this is the inventory canvas
+		}
+		else if(!(inventoryToggle.GetComponent<Toggle>().isOn) ){				// if naka off ang toggle
+			inventoryToggle.transform.GetChild(0).GetChild(0).GetComponent<Image>().enabled = false;
+			inventoryToggle.transform.GetChild(0).GetComponent<Image>().enabled = true;
+			gameObject.GetComponent<Canvas>().enabled = false;			// this is the inventory canvas
+		}
+	}
+}
--- Assets/shopManager.cs
+++ Assets/shopManager.cs
@@ -13,9 +13,11 @@ public class shopManager : MonoBehaviour {
 	public Text slowPriceText;
 	private Text slowQtyText;
 
-
+	private int lifePotionQty;
 	public int lifePotionPrice;
-
+	public Text lifePotionText;
+	private Text lifePotionQtyText;
+	
 	void Awake(){
 		if( !(PlayerPrefs.HasKey("slow qty:")) ){	// if wala pa. meaning 1st time ng user sa shop, 1st time mag laro
 			PlayerPrefs.SetInt("slow qty:", 0);
@@ -32,13 +34,19 @@ public class shopManager : MonoBehaviour {
 		money = PlayerPrefs.GetInt("Money");
 		moneyText.text = "₱" + money;
 
-
 		slowPriceText.text = "₱" + slowPrice;
 		slowQty = PlayerPrefs.GetInt("slow qty:");
 		slowQtyText = slowPriceText.transform.parent.parent.GetChild(2).GetChild(0).GetComponent<Text>();	// the qty gameObject of the item
 		slowQtyText.text = "" + slowQty + "/3";		
 
-		//PlayerPrefs.SetInt("slow qty:", 0);	// TAGA RESET
+		lifePotionText.text = "₱" + lifePotionPrice;
+		lifePotionQty = PlayerPrefs.GetInt("life potion qty:");
+		lifePotionQtyText = lifePotionText.transform.parent.parent.GetChild(2).GetChild(0).GetComponent<Text>();	// the qty gameObject of the item
+		lifePotionQtyText.text = "" + lifePotionQty + "/3";		
+
+//		PlayerPrefs.SetInt("slow qty:", 0);	// TAGA RESET
+//		PlayerPrefs.SetInt("life potion qty:", 0);	// TAGA RESET
+//		PlayerPrefs.SetInt("Money", 1200);
 		
 	}
 	
@@ -55,7 +63,21 @@ public class shopManager : MonoBehaviour {
 			slowQtyText.text = "" + slowQty + "/3";					// update the qty txt gameObject of the item
 			Debug.Log(slowQty);
 			PlayerPrefs.SetInt("Money", money - slowPrice);			// update the money variable, deduct price of item
-			moneyText.text = PlayerPrefs.GetInt("Money").ToString();// then display it again
+			money = PlayerPrefs.GetInt("Money");
+			moneyText.text = "₱" + money;// then display it again
+		}
+	}
+
+	public void buylifePotion(){
+		if(money >= lifePotionPrice && lifePotionQty < 3){						// To set the max to 3, less than 3 lang dapat walang equal. idk why
+			
+			PlayerPrefs.SetInt("life potion qty:", lifePotionQty + 1);			// add the qty of the item
+			lifePotionQty = PlayerPrefs.GetInt("life potion qty:");				// store again on the variable
+			lifePotionQtyText.text = "" + lifePotionQty + "/3";					// update the qty txt gameObject of the item
+			Debug.Log(lifePotionQty);
+			PlayerPrefs.SetInt("Money", money - lifePotionPrice);			// update the money variable, deduct price of item
+			money = PlayerPrefs.GetInt("Money");
+			moneyText.text = "₱" + money;// then display it again
 		}
 	}
 

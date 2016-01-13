c7547483048acc383a7d38b4d461d9634b203220
goldenVoice <goldenVoice8@gmail.com>
2016-01-13 23:18:20 +0800

    carrot cannon: working on

---
+++ Assets/CarrotCannon_Impact_Behavior.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarrotCannon_Impact_Behavior : MonoBehaviour {
+
+	public Transform range_center;
+	public CircleCollider2D range_radius;
+	public LayerMask enemyLayerMask;
+
+  	public GameObject hero;
+  	public float speed = 10;
+  	public float damage;
+  	public GameObject target;
+  	
+  	ElementManager elementManager;
+  	ElementManager.Element hero_element;
+
+  	private GameManagerBehavior gameManager;    // rewards player when they destroy the enemy
+
+ 	public GameObject bulletImpact_particle;
+
+	private Collider2D[] enemies;
+
+	// Use this for initialization
+	void Start () {
+		elementManager = (ElementManager) FindObjectOfType(typeof(ElementManager));
+		hero_element = hero.transform.GetChild(1).GetComponent<HeroData>().heroElement;
+    	GameObject gm = GameObject.Find("GameManager");
+		gameManager = gm.GetComponent<GameManagerBehavior>();
+		
+	}
+
+	void FixedUpdate(){
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+		enemies = Physics2D.OverlapCircleAll(range_center.position, range_radius.radius, enemyLayerMask);
+
+		for (int i = 0; i < enemies.Length; i++){
+			Debug.Log(enemies[i].name);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+
+			// if walking ang enemy. deduct. kase 
+			if(other.GetComponent<EnemyData>().insectPath == EnemyData.pathWay.walking){
+			Transform healthBarTransform = other.transform.parent.FindChild("HealthBar");
+			HealthBar healthBar = healthBarTransform.gameObject.GetComponent<HealthBar>();
+			damage = elementManager.checkElement(hero_element, other.GetComponent<EnemyData>().enemyElement, damage); 	// ex: fire defeats air: damage x 2
+			Debug.Log("Damage: " + damage);
+			Debug.Log("Health before the damage: " + healthBar.currentHealth);
+			healthBar.currentHealth -= Mathf.Max(damage, 0);
+			Debug.Log("after: " + healthBar.currentHealth);
+			Debug.Log("ilang bes to friend:?");
+   				if(healthBar.currentHealth <= 0){
+					// dahil yung mismong parent na enemy gameObject ang i destroy para mawala yung lahat ng components ng enemy
+       				Destroy(other.transform.parent.gameObject);
+       				// code below, mag play ng sound ng enemy pag namatay, KUNG may sound sa gameObject na enemy
+       				//AudioSource audioSource = target.GetComponent<AudioSource>();
+       				//AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+		
+       				// reward the user water when the enemy is destroyed
+       				gameManager.water += other.GetComponent<EnemyData>().waterRewarded;
+       				gameManager.displayWater();
+   				}
+
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/banana_HeroAttack.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class banana_HeroAttack : MonoBehaviour {
+
+	public Transform range_center;
+	public CircleCollider2D range_radius;
+	public LayerMask enemyLayerMask;
+
+	public List<GameObject> enemiesInRange;
+
+	private float lastShotTime;
+	private HeroData heroData;
+
+	private GameObject target;
+
+	private GameObject bulletPrefab;
+
+	private AudioSource attackSound;
+//	public GameObject bulletPrefab;
+	
+	private finishedPlanted_carrot planted_carrotScript;
+
+	Animator anim;
+
+	public GameObject firePoint;		// kung san mag fi fire yung banana.
+
+	void Awake(){
+		attackSound = gameObject.GetComponent<AudioSource>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		enemiesInRange = new List<GameObject>();
+		// at the start, there are no enemies, so you create an empty list
+		GameObject parent_hero = gameObject.transform.parent.gameObject;
+    	anim = (Animator)parent_hero.transform.GetChild(1).GetComponent<Animator>(); 
+		lastShotTime = Time.time;
+		heroData = (HeroData)parent_hero.transform.GetChild(1).GetComponent<HeroData>();
+	}
+	
+	void FixedUpdate(){
+		// arguments:			     (center of the circle, radius of the circle, layerMask for filtering objects)
+		Physics2D.OverlapCircle(range_center.position, range_radius.radius, enemyLayerMask);
+//		Debug.Log(Physics2D.OverlapCircle(range_center.position, range_radius.radius, enemyLayerMask) );
+		
+	}
+	// Update is called once per frame
+	void Update () {
+
+		// the hero 
+
+			 target = null;
+			// 
+			float minimalEnemyDistance = float.MaxValue;		// the maximum possible distance
+			foreach(GameObject enemy in enemiesInRange){		// iterate through the list of enemies
+				float distanceToGoal = enemy.transform.parent.GetComponent<MoveEnemy>().distanceToGoal();		// get the distanceToGoal of the current enemy
+				if(distanceToGoal < minimalEnemyDistance){	// kapag yung distance to the end of the stage area (yung goal) ay mas maliit sa minimalEnemyDistance
+					target = enemy;
+					minimalEnemyDistance = distanceToGoal;		// set as new minimal distance.
+				}
+			}	
+
+			if(target != null){
+				if(target.transform.position.x < gameObject.transform.position.x){
+							
+							anim.SetBool("enemy_at_leftSide", true);		// make the hero face left
+							anim.SetBool("enemy_at_rightSide", false);
+							
+					if(Time.time - lastShotTime > heroData.fireRate && checkTarget(target) ){
+						//	anim.SetTrigger("attack");
+						anim.SetTrigger("attack_left");
+						playSound();
+						Shoot(target.GetComponent<Collider2D>(), firePoint);	// function shoot, the targets collider2D is used as parameter
+						lastShotTime = Time.time;
+					}
+
+				}
+				else if(target.transform.position.x > gameObject.transform.position.x){
+								anim.SetBool("enemy_at_rightSide", true);		// make the hero face right
+								anim.SetBool("enemy_at_leftSide", false);
+
+				if(Time.time - lastShotTime > heroData.fireRate && checkTarget(target) ){
+						//	anim.SetTrigger("attack");
+						anim.SetTrigger("attack");
+						playSound();
+						Shoot(target.GetComponent<Collider2D>(), firePoint );	// function shoot, the targets collider2D is used as parameter
+						lastShotTime = Time.time;
+					}
+				}
+
+//				else if(anim.GetTrigger("enemy_at_rightSide")){
+//						print("enemy in the right side");
+//				}
+				
+				// rotate the hero, depending where the target is, 
+				// in your case, pwedeng dito mo ilagay yung code para malaman kung which side haharap yung hero, (left or right)
+					
+//				Vector3 direction = gameObject.transform.position - target.transform.position;
+//				gameObject.transform.rotation = Quaternion.AngleAxis(
+//					Mathf.Atan2 (direction.y, direction.x) * 180 / Mathf.PI;
+//					new Vector3 (0,0,1) );
+			}
+	}
+
+	void OnEnemyDestroy (GameObject enemy){
+		enemiesInRange.Remove (enemy);
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+	//	Debug.Log(other.gameObject.tag + " has entered the range");
+		if(other.gameObject.tag.Equals("Enemy")) {
+			enemiesInRange.Add(other.gameObject);		// add the enemy that entered the collider, on the list of enemies in range
+			EnemyDestructionDelegate del = other.gameObject.GetComponent<EnemyDestructionDelegate>();
+			del.enemyDelegate += OnEnemyDestroy;		// calls OnEnemyDestroy when the enemy is destroyed, idk kung pano nangyare yon
+
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		if(other.gameObject.tag.Equals("Enemy")){
+			enemiesInRange.Remove(other.gameObject);
+			EnemyDestructionDelegate del = other.gameObject.GetComponent<EnemyDestructionDelegate>();
+			del.enemyDelegate -= OnEnemyDestroy;			// you unregister the enemies in the delegate, now you know whic enemies are in range.
+		}
+	}
+
+	void Shoot (Collider2D target, GameObject firePosition){
+		 bulletPrefab = heroData.bullet;
+
+		 Vector3 startPosition = firePosition.transform.position;
+		 Vector3 targetPosition = target.transform.position;
+		 targetPosition.z = bulletPrefab.transform.position.z;		// so that the bullet would appear above the enemy
+		 startPosition.z = bulletPrefab.transform.position.z;			// bullet should appear below the hero firing it
+
+		 GameObject newBullet = (GameObject)Instantiate(bulletPrefab);
+		 newBullet.transform.position = startPosition;
+		 BulletBehavior bulletComp = newBullet.GetComponent<BulletBehavior>();
+		 bulletComp.target = target.gameObject;
+		 bulletComp.startPosition = startPosition;
+		 bulletComp.targetPosition = targetPosition;
+
+		 // HERE you make the game more interesting
+		 // play a sound, and animate the hero to fire.
+
+		 // Animator animator = hero.getComponent<Animator>();
+		 // animator.SetTrigger("HeroFires");
+		 // AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+		 // audioSource.PlayOneShot(audioSource.clip);
+	}
+
+	// checks if the the insect is kasama sa mga target enemy ng hero (example, flying, walking or both ba yung insect?)
+	bool checkTarget(GameObject target){
+		Debug.Log("insect path: " + target.GetComponent<EnemyData>().insectPath);
+		Debug.Log("hero target: " + heroData.target);
+		if(target.GetComponent<EnemyData>().insectPath == heroData.target){
+			return true; //target
+		}
+		else if(heroData.target == EnemyData.pathWay.both){	// basta both (flying, walking) ang target ng hero, a atakihin nya yung enemy
+			return true; //target
+		}
+		else{
+			enemiesInRange.Remove(target.gameObject);
+			EnemyDestructionDelegate del = target.gameObject.GetComponent<EnemyDestructionDelegate>();
+			del.enemyDelegate -= OnEnemyDestroy;			// you unregister the enemies in the delegate, now you know whic enemies are in range.
+			return false;	// meaning hindi niya target enemy yan :D
+		}
+	}
+
+	void playSound(){
+		if(PlayerPrefs.GetInt("sounds") == 1){		// sounds: ON
+			attackSound.PlayOneShot(attackSound.clip, 0.7f);
+		}
+	}
+
+	void ActivateScript(){
+		this.enabled = true;
+	}
+
+}

146e21d0c1df267fdcd5bf378f2399c34ca126e8
Kotaro <Kotaro>
2016-01-16 09:42:34 -0800

    orange saucer unfinished

---
+++ Assets/saucerLayerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class saucerLayerManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// make the layer name of the sacuer same as its parent, to achieve nakatago saucer effect
+		GameObject orangeHero = gameObject.transform.parent.parent.gameObject;
+		gameObject.renderer.sortingLayerName = orangeHero.renderer.sortingLayerName;	
+		orangeHero.transform.GetChild(0).renderer.sortingLayerName = orangeHero.renderer.sortingLayerName;	
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

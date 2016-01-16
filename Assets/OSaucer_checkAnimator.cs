bdbd567cce6b9eaaa14bb8c011a54159ef1b9516
Kotaro <Kotaro>
2016-01-15 21:36:18 -0800

    orange saucer bug fixed

--- Assets/OSaucer_checkAnimator.cs
+++ Assets/OSaucer_checkAnimator.cs
@@ -19,6 +19,9 @@ public class OSaucer_checkAnimator : MonoBehaviour {
 	void Update () {
 		if (OSaucerHero_anim.GetCurrentAnimatorStateInfo (0).IsName ("idle") || OSaucerHero_anim.GetCurrentAnimatorStateInfo (0).IsName ("idle_leftSide")) {
 			saucerAnim.enabled = false;
+
+	//		gameObject.transform.Rotate( new Vector3(0f,0f,9f) );
+			transform.eulerAngles = new Vector3(0f,0f,OSaucer_hero.transform.rotation.z);
 		}
 	
 	}
